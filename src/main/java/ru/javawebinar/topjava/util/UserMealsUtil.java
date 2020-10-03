f8bfe736ae993046ea2d291949a5830465e83e52
RobinHood477 <70698011+RobinHood477@users.noreply.github.com>
2020-10-03 21:09:03 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExcess;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,12 +29,29 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        //
-        return null;
+        List<UserMealWithExcess> list = new ArrayList<>();
+        for (UserMeal item : meals){
+            if (TimeUtil.isBetweenHalfOpen(item.getDateTime().toLocalTime(), startTime, endTime)){
+                list.add(new UserMealWithExcess(item.getDateTime(), item.getDescription(), item.getCalories(), isExcess(meals, item.getDateTime(), caloriesPerDay)));
+            }
+        }
+        return list;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        //
-        return null;
+        List<UserMealWithExcess> out = new ArrayList<>();
+        meals.stream().filter(x -> TimeUtil.isBetweenHalfOpen(x.getDateTime().toLocalTime(), startTime, endTime))
+                .forEach(x -> out.add(new UserMealWithExcess(x.getDateTime(), x.getDescription(), x.getCalories(), isExcess(meals, x.getDateTime(), caloriesPerDay))));
+        return out;
+    }
+
+    private static boolean isExcess(List<UserMeal> meals, LocalDateTime time, int caloriesPerDay){
+        int calories = 0;
+        for (UserMeal item : meals) {
+            if (item.getDateTime().getDayOfMonth() == time.getDayOfMonth()) {
+                calories += item.getCalories();
+            }
+        }
+        return calories > caloriesPerDay;
     }
 }
